dcef48974aef4ce64eede3c48b992405799bf3f5
Karsten Schmidt <k@postspectacular.com>
2015-10-13 21:09:49 +0100

    update deps

--- geom/project.clj
+++ geom/project.clj
@@ -3,12 +3,12 @@
   :url          "https://github.com/thi-ng/demos"
   :license      {:name "Apache Software License"
                  :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/clojurescript "0.0-3117"]
-                 [thi.ng/geom "0.0.777"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.145"]
+                 [thi.ng/geom "0.0.881"]
                  [thi.ng/domus "0.1.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.5"]]
+  :plugins [[lein-cljsbuild "1.1.0"]]
 
   :clean-targets ^{:protect false} ["resources/public/js"]
 
